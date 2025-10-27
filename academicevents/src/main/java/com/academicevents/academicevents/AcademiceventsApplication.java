45d91ff2abb7cbc64c28185fb2f4cc1017b71b3b
Tu Nombre <tu@correo.com>
2025-10-26 22:40:22 -0500

    feat: initial Spring Boot project structure

---
+++ academicevents/src/main/java/com/academicevents/academicevents/AcademiceventsApplication.java
@@ -0,0 +1,13 @@
+package com.academicevents.academicevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiceventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiceventsApplication.class, args);
+	}
+
+}
---
+++ academicevents/src/test/java/com/academicevents/academicevents/AcademiceventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academicevents.academicevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiceventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
